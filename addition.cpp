407cafc089deac10da2965e33d66eb07e8fdb726
DeepSalunkhe <deepsalunkhee@gmail.com>
2022-03-26 15:42:43 +0530

    Test code

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"entre a\n";
+    cin>>a;
+    cout<<"entre b\n";
+    cin>>b;
+    int sum=a+b;
+
+    cout<<sum<<endl;
+
+    return 0;
+
+
+}
