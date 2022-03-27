cf70748717770abd5897fd87ceab064eb285cf0c
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2022-03-27 16:18:44 +0530

    Half pyramid starting with sapce

--- halfpyramipStartingwithspace.cpp
+++ halfpyramipStartingwithspace.cpp
@@ -7,12 +7,16 @@ int main()
  cin>>n;
  for(int i=0;i<=n;i++)
  {
-   for(int j=0;j<=n;j++)
-   {
-     if(j<n-i)
-     { 
-       cout<<" "; 
-     }
+   for(int j=05;j<=n;j++)
+     {
+       if( j<n-i)
+      {
+       cout<<" ";
+
+      }
+
+     
+     
      else
      {
        cout<<"*";
