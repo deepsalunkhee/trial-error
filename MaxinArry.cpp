17875acc4b56242dc6cf91ebeb152d92c976ad73
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2022-03-29 08:44:39 +0530

    Max in arry one by one

---
+++ MaxinArry.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"entre n:";
+    cin>>n;
+    
+    
+    int arr[n];
+     for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int mx=-199999999;
+    for(int i=0;i<n;i++)
+    {
+        mx=max(mx,arr[i]);
+        cout<<mx<<endl;
+    }
+    return 0;
+}
