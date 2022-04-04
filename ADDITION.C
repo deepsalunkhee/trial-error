e44ecc558284ac0ec3e281368b1f977153a02417
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2022-04-04 20:35:04 +0530

    TURBO WTF

---
+++ ADDITION.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+
+
+printf("Entre a:");
+int a;
+scanf("%d",&a);
+
+printf("Entre b:");
+int b;
+scanf("%d",&b);
+printf("THe sum of the given numbers is %d\n",a+b);
+getch();
+return 0;
+}
